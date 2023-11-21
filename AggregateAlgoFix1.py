08959ea55497181b63eebf39b0b0e7e8757ca005
sd100596 <84270517+sd100596@users.noreply.github.com>
2023-11-21 14:18:32 +0530

    Add files via upload

---
+++ AggregateAlgoFix1.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from urllib import request
+import os
+import json
+from io import StringIO
+from pathlib import Path
+
+path_input = Path(os.environ.get("INPUTS", "/data/inputs/algoCustomData.json"))
+path_output = Path(os.environ.get("OUTPUTS", "/data/outputs"))
+path_logs = Path(os.environ.get("LOGS", "/data/logs"))
+
+path_aggregate_output_file = path_output / 'aggregateOutput.csv'
+
+algoCustomData = {}
+
+with open(path_input, "r") as json_file:
+    algoCustomData = json.load(json_file)
+
+urls = algoCustomData["resultUrls"]
+
+def get_data_from_url(url):
+    try:
+        response = request.urlopen(url)
+        if response.getcode() == 200:
+            csv_data = response.read().decode('utf-8')
+            df = pd.read_csv(StringIO(csv_data))
+            return df
+        else:
+            return None
+    except Exception as e:
+        print("Error fetching data from URL:", e)
+        return None
+
+data_frames = [get_data_from_url(url) for url in urls]
+
+if data_frames:
+    combined_df = pd.concat(data_frames, ignore_index=True) 
+    column_averages = combined_df.mean().to_frame().T  
+    column_averages.columns = combined_df.columns
+    column_averages.to_csv(path_aggregate_output_file, index=False)
